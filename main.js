5644ba0806c278d2d194cfb717b2b4885881e715
Olivia <oliviatmanalastas@gmail.com>
2022-07-12 15:10:05 -0400

    Added JavaScript display info

--- main.js
+++ main.js
@@ -2,15 +2,57 @@ const countrySymbol = document.querySelector('.country-symbol')
 const cryptoRate = document.querySelector('.crypto-rate')
 const cryptoDescription = document.querySelector('.crypto-description');
 
-let cryptoLink = 'https://api.coindesk.com/v1/bpi/currentprice.json'
-fetch(cryptoLink)
-.then(res => res.json())
-.then(data => data.forEach(data => {showBPI(data)}))
+const cryptoData = async () => {
+    let req = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
+    let res = await req.json()
+    let container = res.bpi
+    console.log(container)
+    showBPI(container)
+}
+cryptoData()
 
-let showBPI = (data) => {
-    countrySymbol.textContent = data.symbol
-    cryptoRate.textContent = data.rate
-    cryptoDescription.textContent = data.description
+let showBPI = (container) => {
+    let usdData = container.USD
+    let gbpData = container.GBP
+    let eurData = container.EUR
+    usdShowData(usdData)
+    gbpShowData(gbpData)
+    eurShowData(eurData)
+}
+let usdShowData = (usdData) => {
+    const usdBtn = document.querySelector('.usa-country-code')
+    usdBtn.addEventListener('click' , () => {
+        let countrySymbol = document.querySelector('.country-symbol')
+        let cryptoRate = document.querySelector('.crypto-rate')
+        let cryptoDescription = document.querySelector('.crypto-description')
+        countrySymbol.textContent = '$'
+        cryptoRate.textContent = usdData.rate
+        cryptoDescription.textContent = usdData.description
+    })
+}
+
+let gbpShowData = (gbpData) => {
+    const gbpBtn = document.querySelector('.gbp-country-code')
+    gbpBtn.addEventListener('click' , () => {
+        let countrySymbol = document.querySelector('.country-symbol')
+        let cryptoRate = document.querySelector('.crypto-rate')
+        let cryptoDescription = document.querySelector('.crypto-description')
+        countrySymbol.textContent = '£'
+        cryptoRate.textContent = gbpData.rate
+        cryptoDescription.textContent = gbpData.description
+    })
+}
+
+let eurShowData = (eurData) => {
+    const eurBtn = document.querySelector('.eur-country-code')
+    eurBtn.addEventListener('click' , () => {
+        let countrySymbol = document.querySelector('.country-symbol')
+        let cryptoRate = document.querySelector('.crypto-rate')
+        let cryptoDescription = document.querySelector('.crypto-description')
+        countrySymbol.textContent = '€'
+        cryptoRate.textContent = eurData.rate
+        cryptoDescription.textContent = eurData.description
+    })
 }
 
 const fundsBtn = document.querySelector('.fundsBtn')
@@ -32,6 +74,12 @@ buyBtn.addEventListener('submit' , (e) => {
     p.textContent = buyStocks.value
     buyContainer.appendChild(p)
 })
+
+let cryptoCurrency = () => {
+    let cryptoList = document.querySelector('#crypto-list')
+    let buyStocks = document.querySelector('#buy-stocks')
+    buyStocks.value = cryptoList.options[cryptoList.selectedIndex].text
+}
 
 const sellBtn = document.querySelector('.sellBtn')
 const sellStocks = document.querySelector('#sell-stocks')
